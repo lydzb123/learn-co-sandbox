e2e5aff9103d066c5421fea137c2fef60e96c8e2
Lydia B <lydiabaik@gmail.com>
2020-04-22 05:37:05 +0000

    Automatically backed up by Learn

--- greeting.rb
+++
@@ -1,7 +0,0 @@
-def greeting_programmer(name, language = "computer")
-  puts "Hello, #{name}. We heard you are a great #{language} programmer."
-end
-
-greeting_programmer("Lydia", "Ruby")
-
-greeting_programmer( ,)
--- sonnet_generator/lib/sonnet_generator.rb
+++
@@ -1,6 +0,0 @@
-require "sonnet_generator/version"
-
-module SonnetGenerator
-  class Error < StandardError; end
-  # Your code goes here...
-end
